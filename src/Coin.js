3c5e6fc3af16b8650f421680e71da30c9fb042e8
agoomar <agoomar1996@gmail.com>
2021-04-25 03:48:46 -0500

    To show price change and market cap

--- src/Coin.js
+++ src/Coin.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 // volume.toLocaleString() shows commas for the prices
 
-const Coin = ({ image, name, symbol, price, volume }) => {
+const Coin = ({
+	image,
+	name,
+	symbol,
+	price,
+	volume,
+	priceChange,
+	marketcap,
+}) => {
 	return (
 		<div className='coin-container'>
 			<div className='coin-row'>
@@ -14,6 +22,16 @@ const Coin = ({ image, name, symbol, price, volume }) => {
 				<div className='coin-data'>
 					<p className='coin-price'>${price}</p>
 					<p className='coin-volume'>${volume.toLocaleString()}</p>
+
+					{priceChange < 0 ? (
+						<p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
+					) : (
+						<p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
+					)}
+
+					<p className='coin-marketcap'>
+						Market Cap: ${marketcap.toLocaleString()}
+					</p>
 				</div>
 			</div>
 		</div>
